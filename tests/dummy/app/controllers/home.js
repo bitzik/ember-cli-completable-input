d90526e6b9f438a9ecfee572bf8723baea14440f
leo jPod <leo.jpod@gmail.com>
2015-03-01 23:46:09 +0100

    more debugging tools let's make it work now :boom:

--- tests/dummy/app/controllers/home.js
+++ tests/dummy/app/controllers/home.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   //Completable input config
+  potentialComplements: [],
   completableValue: null,
   minForComplement: 0,
   complements:
